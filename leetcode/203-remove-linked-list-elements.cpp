1ecda7961ff67897d92c0905d4ce292ffa06f38a
James Zhou <yunchengzhou@gmail.com>
2026-04-09 21:59:32 -0400

    203. Remove Linked List Elements

---
+++ leetcode/203-remove-linked-list-elements.cpp
@@ -0,0 +1,28 @@
+struct ListNode {
+    int val;
+    ListNode *next;
+    //ListNode() : val(0), next(nullptr) {}
+    //ListNode(int x) : val(x), next(nullptr) {}
+    //ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode *curr = head;
+        ListNode *prev = nullptr;
+        while (curr != nullptr) {
+            if (curr->val == val) {
+                if (curr == head) {
+                    head = head->next;
+                } else {
+                    prev->next = curr->next; 
+                }
+            } else {
+                prev = curr;
+            }
+            curr = curr->next;
+        }
+        return head;
+    }
+};

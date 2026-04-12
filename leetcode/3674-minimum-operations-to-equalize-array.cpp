7a91620258145c6d9cba19b6705f62a10caeffc3
James Zhou <yunchengzhou@gmail.com>
2026-04-12 12:51:46 -0400

    stupid solution

---
+++ leetcode/3674-minimum-operations-to-equalize-array.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int temp = nums[0];
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] != temp) {
+                return 1;
+            }
+        }
+        return 0;
+    }
+};

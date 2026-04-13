a50ec2a2cc0f8d163d151f421a1117074ae22320
James Zhou <yunchengzhou@gmail.com>
2026-04-13 13:38:30 -0400

    347. Top K Frequent Elements

---
+++ leetcode-med/347-top-k-frequent-elements.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <unordered_map>
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for (int i = 0; i < nums.size(); i++) {
+            freq[nums[i]]++;
+        }
+        vector<vector<int>> bucket(nums.size());
+        for (const auto& [key, value] : freq) {
+            bucket[value-1].push_back(key);
+        }
+        int index = bucket.size()-1;
+        int count = 0;
+        vector<int> result;
+        while (index >= 0 && count < k) {
+            int i = 0;
+            count += bucket[index].size();
+            while (i < bucket[index].size()) {
+                result.push_back(bucket[index][i]);
+                i++;
+            }
+            index--;
+        }
+        return result;
+    }
+};

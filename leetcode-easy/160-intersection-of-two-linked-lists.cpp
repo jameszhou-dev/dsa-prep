64213c4b765c07f1769057719f88b0472d969c9f
James Zhou <yunchengzhou@gmail.com>
2026-04-13 14:18:11 -0400

    160. Intersection of Two Linked Lists

---
+++ leetcode-easy/160-intersection-of-two-linked-lists.cpp
@@ -0,0 +1,27 @@
+#include <unordered_set>
+using namespace std;
+
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        unordered_set<ListNode*> nodes;
+        while (headA != nullptr) {
+            nodes.insert(headA);
+            headA = headA->next;
+        }
+        while (headB != nullptr) {
+            if (nodes.find(headB) != nodes.end()) {
+                return headB;
+            }
+            headB = headB->next;
+        }
+        return nullptr;
+        
+    }
+};

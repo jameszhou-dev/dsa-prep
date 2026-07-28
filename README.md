7e7507bdb55cae12419ffc07ed303d8b367671de
James Zhou <yunchengzhou@gmail.com>
2026-07-28 15:33:01 -0400

    more dp

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LC Stats
 
-![Problems Solved](https://img.shields.io/badge/solved-210-brightgreen)
+![Problems Solved](https://img.shields.io/badge/solved-213-brightgreen)
 ![Language](https://img.shields.io/badge/language-C++-blue)
-![Last Updated](https://img.shields.io/badge/updated-2026--07--27-lightgrey)
+![Last Updated](https://img.shields.io/badge/updated-2026--07--28-lightgrey)
 
--- leetcode/5.cpp
+++ leetcode/5.cpp
@@ -3,20 +3,19 @@
 class Solution {
 public:
   std::string longestPalindrome(std::string s) {
-    std::vector<std::vector<bool>> palindromes(
-        s.size() + 1, std::vector<bool>(s.size() + 1, false));
-    for (int i = 0; i < s.size() + 1; ++i) {
-      palindromes[i][0] = true;
-      palindromes[i][1] = true;
+    vector<vector<bool>> dp(s.size() + 1, vector<bool>(s.size() + 1, false));
+    for (int i = 0; i < s.size(); ++i) {
+      dp[i][0] = true;
+      dp[i][1] = true;
     }
     int start = 0;
     int len = 1;
-    for (int i = 2; i <= s.size(); ++i) {
-      for (int j = 0; j + i - 1 < s.size(); ++j) {
-        if (palindromes[j + 1][i - 2] && s[j + i - 1] == s[j]) {
-          palindromes[j][i] = true;
-          start = j;
-          len = i;
+    for (int j = 2; j <= s.size(); ++j) {
+      for (int i = 0; i <= s.size() - j; ++i) {
+        if (dp[i + 1][j - 2] && s[i] == s[i + j - 1]) {
+          dp[i][j] = true;
+          start = i;
+          len = j;
         }
       }
     }
